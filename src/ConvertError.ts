05c2642ac99b86be0e75d6dc5604fc777d78970a
hubluesky <hubluesky@126.com>
2024-04-25 10:38:02 +0800

    1、修改返回值错误信息

--- src/ConvertError.ts
+++ src/ConvertError.ts
@@ -1,4 +1,4 @@
 
 export class ConvertError extends Error {
-    constructor(code: number, msg: string, ...params: any[]) { super(msg, { cause: code }); }
+    constructor(code: number, msg: string, ...params: any[]) { super(JSON.stringify({ code, msg, params }), { cause: code }); }
 }
